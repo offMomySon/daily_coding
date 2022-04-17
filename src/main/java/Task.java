d867e3b11ce205c9f05421836d3efcdd752cfb44
오지훈 <ohji100629@naver.com>
2022-04-17 23:24:26 +0900

    implement Tag

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,48 +1,21 @@
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 
 public class Task {
+    private static final Tag tag = new Tag();
     private static final int LIMIT = 9;
     private static final boolean[] usingTag = new boolean[LIMIT+1];
 
     public boolean create(){
-        boolean isCreated = false;
-
-        for (int i = 1; i <= LIMIT; i++) {
-            if(usingTag[i] == false){
-                usingTag[i] = true;
-                isCreated = true;
-                break;
-            }
-        }
-
-        return isCreated;
+        return tag.setTagIfPossible();
     }
 
-    public boolean execute(int tag){
-        if(tag > LIMIT){
-            return false;
-        }
-        if(!usingTag[tag]){
-            return false;
-        }
-
-        usingTag[tag] = false;
-
-        return true;
+    public boolean execute(int tagNum){
+        return tag.deleteTag(tagNum);
     }
 
-    public List<Integer> getUsableTags(){
-        List<Integer> usableList = new LinkedList<>();
-        
-        for(int i =1 ; i<=LIMIT; i++){
-            if(!usingTag[i]){
-                usableList.add(i);
-            }
-        }
-
-        return usableList;
+    public Tag getTag(){
+        return tag;
     }
 }
