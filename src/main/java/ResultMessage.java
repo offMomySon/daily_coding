b9f0fcdf70deba5dc73c9e0e99a19c2f23bf0aed
오지훈 <ohji100629@naver.com>
2022-04-17 21:03:31 +0900

    implement ResultMessage

---
+++ src/main/java/ResultMessage.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ResultMessage {
+    private static final String usableTagFormat = "사용가능한 Tag : %s";
+    private static final String createFailFormat = "TASK 생성 실패: %d";
+    private static final String executeFailFormat = "TASK 수행 실패한 태그: %s";
+
+    public void printUsableTag(List<Integer> usableList){
+        System.out.println(String.format(usableTagFormat, usableList.stream().map(it-> Integer.toString(it)).collect(Collectors.joining(" "))));
+    }
+
+    public void printCreateFail(int createFailCount){
+        System.out.println(String.format(createFailFormat, createFailCount));
+    }
+
+    public void printExecuteFails(Map<Integer, ExecuteFail> executeFails){
+        String collect = executeFails.values().stream().sorted().map(it -> it.getNum() + "(" + it.getCount() + ")").collect(Collectors.joining(" "));
+        System.out.println(String.format(executeFailFormat,collect));
+    }
+}
