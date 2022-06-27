8f44ed17d877a1819ae9dba7d630113e664bcd7c
오지훈 <ohji100629@naver.com>
2022-06-27 17:20:26 +0900

    Impl commandFactory

---
+++ src/main/java/taskjob/CommandFactory.java
@@ -0,0 +1,35 @@
+package taskjob;
+
+import java.text.MessageFormat;
+
+public class CommandFactory {
+    private static final String CMD_DELIMITER = " ";
+
+    private static final int COMMAND_MIN_LENGTH = 1;
+    private static final int EXECUTE_COMMAND_MIN_LENGTH = 2;
+
+    public Command create(String sCmd){
+        String[] splitCmd = sCmd.split(CMD_DELIMITER);
+
+        if( splitCmd.length < COMMAND_MIN_LENGTH ){
+            throw new RuntimeException(MessageFormat.format("command min length is {}, current length is {}", COMMAND_MIN_LENGTH, splitCmd.length));
+        }
+
+
+        Cmd cmd = Cmd.find(splitCmd[0]).orElseThrow(() -> new RuntimeException("cmd not exist"));
+
+
+        switch (cmd){
+            case CREATE:
+                return new CreateCommand();
+            case EXECUTE:
+                if(splitCmd.length < EXECUTE_COMMAND_MIN_LENGTH){
+                    throw new RuntimeException(MessageFormat.format("execute command min length is {}, current length is {}", EXECUTE_COMMAND_MIN_LENGTH, splitCmd.length));
+                }
+                Task requestTask = Task.from(splitCmd[1]);
+                return new ExecuteCommand(requestTask);
+        }
+
+        throw new RuntimeException(MessageFormat.format("fail to create cmd. sCmd is `{}`", sCmd));
+    }
+}
