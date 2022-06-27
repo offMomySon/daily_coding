a39c81d717190ee669c3d2750f09ca576490045e
오지훈 <ohji100629@naver.com>
2022-06-27 20:44:57 +0900

    Apply TaskCounter

---
+++ src/main/java/taskjob/TaskCount.java
@@ -0,0 +1,26 @@
+package taskjob;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import lombok.NonNull;
+
+public class TaskCount {
+    private final Map<Task, Integer> groupByTaskCount;
+
+    public TaskCount() {
+        this.groupByTaskCount = new HashMap<>();
+    }
+
+    public void add(@NonNull Task task){
+        if(groupByTaskCount.containsKey(task)){
+            groupByTaskCount.put(task, groupByTaskCount.get(task) + 1);
+        }
+
+        groupByTaskCount.put(task, 1);
+    }
+
+    public Map<Task, Integer> getTaskCountsAsView() {
+        return new HashMap<>(groupByTaskCount);
+    }
+}
---
+++ src/test/java/taskjob/TaskCountTest.java
@@ -0,0 +1,35 @@
+package taskjob;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskCountTest {
+
+    @DisplayName("입력 받은 task 횟수만큼 count 가 증가합니다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1", "2,2", "3,3,3"})
+    void test1(String sTags){
+        //given
+        List<Task> tasks = Arrays.stream(sTags.split(",")).map(Task::from).collect(Collectors.toUnmodifiableList());
+        Map<Task, Integer> expect = Map.of(tasks.get(0), tasks.size());
+
+        TaskCount taskCount = new TaskCount();
+        tasks.forEach(taskCount::add);
+
+        //when
+        Map<Task, Integer> actual = taskCount.getTaskCountsAsView();
+
+        //then
+        Assertions.assertThat(actual)
+            .isEqualTo(expect);
+    }
+}
